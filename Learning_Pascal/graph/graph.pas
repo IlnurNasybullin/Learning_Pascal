91aec7bb745a50a87f3397fada32002bc6e1105a
Ilnur Nasybullin <ilnur.nasybullin2000@yandex.ru>
2019-05-14 22:05:11 +0300

    Created on 14th of May 2019 at 22:04 version 3.0

--- Learning_Pascal/graph/graph.pas
+++ Learning_Pascal/graph/graph.pas
@@ -1,71 +1,39 @@
 ﻿Program graph;
 
-Uses graphEdgeList;
-Uses graphEdgeSet;
-Uses myDates;
+Uses myEdgeList;
 
-Procedure fillEdges(edgeStr: string; ID_one:node_ID);//заполнение рёбер из матрицы инцидентности
-const IS_EDGE = '1';
-      IS_NOT_EDGE = '0';
-var i:integer;
-    ID_two:node_ID;
+Const INPUT_FILE_NAME = 'input.txt';
+      OUTPUT_FILE_NAME = 'output.txt';
+      
+Var pEdgeList: pointList;
+    pEdgeSetList: pointList;
+
+Procedure getSetEdges(var pEdgeSetList: pointList; pEdgeList: pointList);
+var metk:pointList;
+    nearEdge:pointList;
 begin
-    ID_two:=1;
-    For i:=1 to length(edgeStr) do begin
-        If (edgeStr[i] = IS_EDGE) and not isExistEdge(ID_two, ID_one) then addEdge(ID_one, ID_two);//добвление ребра в список рёбер
-        If (edgeStr[i] = IS_EDGE) or (edgeStr[i] = IS_NOT_EDGE) then ID_two += 1;
+    metk:=pEdgeList^.next;
+    While isExistNotMetk(pEdgeList) and (metk<>pEdgeList) do begin
+        addPointInTail(pEdgeSetList, metk);
+        setMetk(metk);
+        nearEdge:=getNearNotMetkEdge(pEdgeList,metk);
+        While nearEdge<>pEdgeList do begin
+            setMetk(nearEdge);
+            nearEdge:=getNearNotMetkEdge(pEdgeList,metk);
+        end;
+        While isExistMetkPoint(metk) and (metk<>pEdgeList) do
+            metk:=metk^.next;
     end;
-end;
-
-Procedure getDates();//получение данных из файла
-const FILE_NAME = 'input.txt';
-var f:text;
-    edgeStr:string;
-    ID:node_ID;
-begin
-    Assign(f,FILE_NAME);
-    Reset(f);
-    ID:=1;
-    While not Eof(f) do begin
-        ReadLn(f, edgeStr);
-        fillEdges(edgeStr,ID);//заполнение рёбер из матрицы инцидентности
-        ID+=1;
-    end;
-    Close(f);
-end;
-
-Procedure sortEdges();//сортировка рёбер графа в порядке убывания
-begin
-    setCount();//установление количества соседних рёбер, по которому будут сортировываться рёбра
-    sortByCount();//сортировка
-end;
-
-Procedure getSubset();//получение максимального подмножества
-begin
-    copyEdgeToSet(getEdgeHead());//создание нового списка рёбер из указателя полученного от прежнего списка
-    createSubset();//создание максимального подмножества
-end;
-
-Procedure printAnswer();//запись рёбер максимального подмножества в отдельный файл
-const FILE_NAME='output.txt';
-var edgeSubset:pointEdgeSet;
-    metk:pointEdgeSet;
-    f:text;
-begin
-    edgeSubset:=getEdgeSubsetHead();//получение указателя начало подмножества рёбер
-    metk:=edgeSubset^.next;
-    Assign(f,FILE_NAME);
-    Rewrite(f);
-    While metk<>edgeSubset do begin
-        WriteLn(f,metk^.ID_nodeOne,' ',metk^.ID_nodeTwo);
-        metk:=metk^.next;
-    end;
-    Close(f);
 end;
 
 Begin
-    getDates();//получение данных из файла
-    sortEdges();//сортировка рёбер графа в порядке убывания
-    getSubset();//получение максимального подмножества
-    printAnswer();//запись рёбер максимального подмножества в отдельный файл
+    pEdgeList:=getNewPointList();
+    fillList(pEdgeList, INPUT_FILE_NAME);
+    setCountNearEdges(pEdgeList);
+    sortEdgesByCountFromMinToMax(pEdgeList);
+    //sortEdgesByCountFromMaxToMin(pEdgeList);
+    pEdgeSetList:=getNewPointList();
+    getSetEdges(pEdgeSetList, pEdgeList);
+    printList(pEdgeSetList, OUTPUT_FILE_NAME);
 End.
+      
---
+++ Learning_Pascal/graph/myEdgeList.pas
@@ -0,0 +1,258 @@
+﻿Unit myEdgeList;
+
+Interface
+
+Type pointList = ^edgeList;
+     edgeList = record
+        nodeOneID: string;
+        nodeTwoID: string;
+        countNearEdges: integer;
+        isMetk: boolean;
+        next: pointList;
+     end;
+
+Var pListHead: pointList;
+
+Function getNewPointList():pointList;
+Procedure addPointInTail(var pList: pointList; metk: pointList);
+Procedure fillList(var pList:pointList; FILE_NAME: string);
+Procedure setMetk(var pList: pointList);
+Procedure setCountNearEdges(var pList: pointList);
+Function isExistNotMetk(pList: pointList):boolean;
+Function isExistMetkPoint(pList: pointList):boolean;
+Function getNearNotMetkEdge(pList: pointList; edge:pointList):pointList;
+Procedure sortEdgesByCountFromMinToMax(var pList: pointList);
+Procedure sortEdgesByCountFromMaxToMin(var pList: pointList);
+Procedure printList(pList: pointList; FILE_NAME:string);
+
+Implementation
+
+Procedure initList(var pList: pointList);
+begin
+    new(pList);
+    pList^.next:=pList;
+end;
+
+Function getNewPointList():pointList;
+begin
+    initList(pListHead);
+    getNewPointList:=pListHead;
+end;
+
+Procedure addTail(var pList: pointList; nodeOneID: string; nodeTwoID: string);
+var y: pointList;
+begin
+    new(y);
+    y^.next:=pList^.next;
+    pList^.nodeOneID:=nodeOneID;
+    pList^.nodeTwoID:=nodeTwoID;
+    pList^.countNearEdges:=0;
+    pList^.isMetk:=false;
+    pList^.next:=y;
+    pList:=y;
+end;
+
+Function getLastPoint(pList: pointList):pointList;
+var metk:pointList;
+begin
+    metk:=pList^.next;
+    While metk^.next<>pList do
+        metk:=metk^.next;
+    getLastPoint:=metk;
+end;
+
+Procedure copyPointDates(var pList: pointList; metk: pointList);
+begin
+    pList^.nodeOneID:=metk^.nodeOneID;
+    pList^.nodeTwoID:=metk^.nodeTwoID;
+    pList^.countNearEdges:=metk^.countNearEdges;
+    pList^.isMetk:=metk^.isMetk;
+end;
+
+Procedure addPointInTail(var pList: pointList; metk: pointList);
+var lastPoint:pointList;
+begin
+    addTail(pList, metk^.nodeOneID, metk^.nodeTwoID);
+    lastPoint:=getLastPoint(pList);
+    copyPointDates(lastPoint, metk);
+end;
+
+Procedure getParseStr(str: string; var stringOne: string; var stringTwo: string);
+
+const SPACE = ' ';
+
+var i:integer;
+
+begin
+    stringOne:='';
+    stringTwo:='';
+    i:=1;
+    While (i<=length(str)) and (str[i]<>SPACE) do
+        i+=1;
+    stringOne:=copy(str, 1, i-1);
+    stringTwo:=copy(str, i+1, length(str)-i);
+end;
+
+Procedure fillList(var pList:pointList; FILE_NAME: string);
+var f:text;
+    str: string;
+    nodeOneID: string;
+    nodeTwoID: string;
+begin
+    Assign(f, FILE_NAME);
+    Reset(f);
+    While not Eof(f) do begin
+        ReadLn(f, str);
+        getParseStr(str, nodeOneID, nodeTwoID);
+        addTail(pList, nodeOneID, nodeTwoID);
+    end;
+    Close(f);
+end;
+
+Procedure setMetk(var pList: pointList);
+begin
+    pList^.isMetk:=true;
+end;
+
+Function isNearEdges(edgeOne: pointList; edgeTwo: pointList):boolean;
+begin
+    isNearEdges:=(edgeOne<>edgeTwo) and ((edgeOne^.nodeOneID=edgeTwo^.nodeOneID)or(edgeOne^.nodeOneID=edgeTwo^.nodeTwoID)
+    or(edgeOne^.nodeTwoID=edgeTwo^.nodeOneID)or(edgeOne^.nodeTwoID=edgeTwo^.nodeTwoID));
+end;
+
+Procedure setCountNearEdges(var pList: pointList);
+var metkOne:pointList;
+    metkTwo:pointList;
+begin
+    metkOne:=pList^.next;
+    While metkOne<>pList do begin
+        metkTwo:=metkOne^.next;
+        While metkTwo<>pList do begin
+            If isNearEdges(metkOne, metkTwo) then begin
+                metkOne^.countNearEdges+=1;
+                metkTwo^.countNearEdges+=1;
+            end;
+            metkTwo:=metkTwo^.next;
+        end;
+        metkOne:=metkOne^.next;
+    end;
+end;
+
+Function isLess(edgeOne: pointList; edgeTwo: pointList):boolean;
+begin
+    isLess:=edgeOne^.countNearEdges<edgeTwo^.countNearEdges;
+end;
+
+Function isExistNotMetk(pList: pointList):boolean;
+var metk:pointList;
+    f:boolean;
+begin
+    metk:=pList^.next;
+    f:=not metk^.isMetk;
+    While (metk<>pList) and not f do begin
+        metk:=metk^.next;
+        f:=not metk^.isMetk;
+    end;
+    isExistNotMetk:=f;
+end;
+
+Function isExistMetkPoint(pList: pointList):boolean;
+begin
+    isExistMetkPoint:=pList^.isMetk;
+end;
+
+Function getNearNotMetkEdge(pList: pointList; edge:pointList):pointList;
+var metk:pointList;
+    f:boolean;
+begin
+    metk:=pList^.next;
+    f:=not isExistMetkPoint(metk) and (edge<>metk) and ((edge^.nodeOneID=metk^.nodeOneID)or(edge^.nodeOneID=metk^.nodeTwoID)
+    or(edge^.nodeTwoID=metk^.nodeOneID)or(edge^.nodeTwoID=metk^.nodeTwoID));
+    while (metk<>pList) and not f do begin
+        metk:=metk^.next;
+        f:=not isExistMetkPoint(metk) and (edge<>metk) and ((edge^.nodeOneID=metk^.nodeOneID)or(edge^.nodeOneID=metk^.nodeTwoID)
+        or(edge^.nodeTwoID=metk^.nodeOneID)or(edge^.nodeTwoID=metk^.nodeTwoID));
+    end;
+    getNearNotMetkEdge:=metk;
+end;
+
+Procedure exchangePointDates(var pListOne: pointList; var pListTwo: pointList);
+var metk:pointList;
+begin
+    new(metk);
+    metk^.nodeOneID:=pListOne^.nodeOneID;
+    metk^.nodeTwoID:=pListOne^.nodeTwoID;
+    metk^.countNearEdges:=pListOne^.countNearEdges;
+    metk^.isMetk:=pListOne^.isMetk;
+    pListOne^.nodeOneID:=pListTwo^.nodeOneID;
+    pListOne^.nodeTwoID:=pListTwo^.nodeTwoID;
+    pListOne^.countNearEdges:=pListTwo^.countNearEdges;
+    pListOne^.isMetk:=pListTwo^.isMetk;
+    pListTwo^.nodeOneID:=metk^.nodeOneID;
+    pListTwo^.nodeTwoID:=metk^.nodeTwoID;
+    pListTwo^.countNearEdges:=metk^.countNearEdges;
+    pListTwo^.isMetk:=metk^.isMetk;
+    dispose(metk);
+end;
+
+Procedure sortEdgesByCountFromMinToMax(var pList: pointList);
+var metk:pointList;
+    lastMetk:pointList;
+    f:boolean;
+begin
+    lastMetk:=pList^.next;
+    metk:=lastMetk^.next;
+    while metk<>pList do begin
+        f:=isLess(metk, lastMetk);
+        If f then begin
+            exchangePointDates(metk, lastMetk);
+            metk:=lastMetk;
+            While lastMetk^.next<>metk do
+                lastMetk:=lastMetk^.next;
+        end;
+        If not f or (lastMetk=pList) then begin
+            lastMetk:=metk;
+            metk:=metk^.next;
+        end;
+    end;
+end;
+
+Procedure sortEdgesByCountFromMaxToMin(var pList: pointList);
+var metk:pointList;
+    lastMetk:pointList;
+    f:boolean;
+begin
+    lastMetk:=pList^.next;
+    metk:=lastMetk^.next;
+    while metk<>pList do begin
+        f:=isLess(lastMetk, metk);
+        If f then begin
+            exchangePointDates(metk, lastMetk);
+            metk:=lastMetk;
+            While lastMetk^.next<>metk do
+                lastMetk:=lastMetk^.next;
+        end;
+        If not f or (lastMetk=pList) then begin
+            lastMetk:=metk;
+            metk:=metk^.next;
+        end;
+    end;
+end;
+
+Procedure printList(pList: pointList; FILE_NAME:string);
+var f:text;
+    metk:pointList;
+begin
+    metk:=pList^.next;
+    Assign(f,FILE_NAME);
+    Rewrite(f);
+    While metk<>pList do begin
+        WriteLn(f,metk^.nodeOneID,' ',metk^.nodeTwoID);
+        metk:=metk^.next;
+    end;
+    Close(f);
+end;
+
+Initialization
+
+End.
